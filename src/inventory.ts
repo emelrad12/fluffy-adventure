9622c4c0921b898c4de295f7d04f5ddc27a4b69d
emelrad12 <emelrad12@gmail.com>
2021-11-15 19:41:57 +0100

    Add basic prices and user order classes

---
+++ src/inventory.ts
@@ -0,0 +1,9 @@
+// including discounted items
+export let prices = {
+    "001" : 100,
+    "002" : 80,
+    "003" : 50,
+    "004" : 30,
+    "-001" : 200,
+    "-002" : 120,
+}
---
+++ src/userOrder.ts
@@ -0,0 +1,14 @@
+export class UserOrder {
+    items: Record<string, number>
+
+    constructor() {
+        this.items = {}
+    }
+
+    applyFromArray(data: Array<string>) {
+        data.forEach((item) => {
+            if (this.items[item]) this.items[item] = 0
+            this.items[item]++
+        })
+    }
+}
