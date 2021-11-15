49d50d07c2fadffc1b6603186c7db1f850b7462c
emelrad12 <emelrad12@gmail.com>
2021-11-15 20:12:52 +0100

    Refactor prices, and add function to apply them

--- src/prices.ts
+++ src/prices.ts
@@ -1,9 +1,25 @@
 // including discounted items
-export let prices = {
-    "001" : 100,
-    "002" : 80,
-    "003" : 50,
-    "004" : 30,
-    "-001" : 200,
-    "-002" : 120,
+import {UserOrder} from "./userOrder";
+
+export class Prices {
+    data: Record<string, number>
+
+    constructor() {
+        this.data = {
+            "001": 100,
+            "002": 80,
+            "003": 50,
+            "004": 30,
+            "-001": 200,
+            "-002": 120,
+        }
+    }
+
+    applyPrices = (order: UserOrder): number => {
+        return Object.keys(order.items).reduce((prev, productName) => {
+            let productCount = order.items[productName]
+            let pricePerProduct = this.data[productName]
+            return prev + productCount * pricePerProduct
+        }, 0)
+    }
 }
