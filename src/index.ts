2a6257f9fbe5e8d7a676d19861a39d64336cbbe0
emelrad12 <emelrad12@gmail.com>
2021-11-15 18:46:47 +0100

    Add express to serve requests

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+import express from "express"
+const app = express()
+const port = 8081
+
+app.get('/checkout', function (req, res) {
+    res.send('Fancy data')
+})
+
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`)
+})
